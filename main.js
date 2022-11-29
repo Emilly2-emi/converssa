57d2b9d47929ca91e30db091d31867deb51fb34c
Emilly2-emi <100239936+Emilly2-emi@users.noreply.github.com>
2022-11-29 18:55:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function addUser{
-    nam_e=document.getElementById("username").value;
-    localStorage.setItem("nome",nam_e);
     
+    window.location="ss.html";
 }
+const firebaseConfig = {
+    apiKey: "AIzaSyCbnwFYl8pKtYHGSuonDg5jCQoBR2_Dreo",
+    authDomain: "conversa-46ba1.firebaseapp.com",
+    databaseURL: "https://conversa-46ba1-default-rtdb.firebaseio.com",
+    projectId: "conversa-46ba1",
+    storageBucket: "conversa-46ba1.appspot.com",
+    messagingSenderId: "901752768980",
+    appId: "1:901752768980:web:e84e9ad514bef6f4216e41",
+    measurementId: "G-ZHFNKSEL8V"
+  };
